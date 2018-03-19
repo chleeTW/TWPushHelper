706bb4e23c11b75af9acb83daf584f4dc3c370e7
이창훈 <chlee@twinny.co.kr>
2018-03-19 22:40:25 +0900

    코드 정리 및 주석 추가

--- app/src/main/java/kr/co/chlee/twpushhelper/PushHelperImpl.java
+++ app/src/main/java/kr/co/chlee/twpushhelper/PushHelperImpl.java
@@ -5,6 +5,14 @@ import android.content.Context;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+/**
+ * @brief 푸시 메시지 처리를 위한 PushHelper 구현 클래스
+ * API 호출 시 필요한 파라미터 값인 start_timestamp을 저장하기 위해서 SharedPreferences를 활용.
+ * 해당 값의 초기화는 로그인 성공 후 동기화된 서버시간을 기준으로 설정함.
+ * @author chlee
+ * @date 2018-02-23
+ */
+
 public abstract class PushHelperImpl implements PushHelper {
 
     private Context mContext;
@@ -12,6 +20,7 @@ public abstract class PushHelperImpl implements PushHelper {
     public PushHelperImpl(Context context) {
         this.mContext = context;
     }
+
     public Context getContext(){
         return mContext;
     }
@@ -20,13 +29,27 @@ public abstract class PushHelperImpl implements PushHelper {
     private Long receivedMqttTime = -1L;
     private boolean lock = false;
 
-    public abstract void apiCall(Long timestamp);
+    /**
+     * Server 에서 마지막으로 성공한(푸시 메시지 처리) 시간부터 새로운 푸시 메시지의 시간까지 해당하는 메시지들을 받아와서 처리하도록 구현해야함
+     * API 응답 성공시에만 마지막으로 성공한 타임스템프를 변경
+     * @param timestamp 호출할 푸시 메시지의 시간
+     */
+    abstract void apiCall(Long timestamp);
 
-    public abstract void setTimestamp(Long timestamp);
 
-    public abstract Long getTimestamp();
+    /**
+     * SharedPreferences에 MQTT 타임스템프를 저장하도록 구현해야함
+     * @param timestamp 새로운 MQTT의 시간
+     */
+    abstract void setTimestamp(Long timestamp);
 
-    public abstract void mqttHandle(JSONObject message) throws JSONException;
+
+    /**
+     * SharedPreferences에 저장된 MQTT 타임스템프를 반환하도록 구현해야함
+     */
+    abstract Long getTimestamp();
+
+    public abstract void handlePushMessage(JSONObject message) throws JSONException;
 
     @Override
     public synchronized void putPush(JSONObject payload) throws JSONException {
